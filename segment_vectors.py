ce054d2c9e31a01a50a5283d8cd5ae8cdeaa7416
Jonathan Lin <nthanjolin@gmail.com>
2020-05-03 03:39:44 -0400

    make k_ising on n-vectors modular

--- segment_vectors.py
+++ segment_vectors.py
@@ -9,159 +9,153 @@ from tqdm import tqdm
 from os import path
 from itertools import permutations
 
-DATA = path.join("iris", "bezdekIris.data") # path to data file
+def ising_k(vectors, labels, show_plot=True):
+    if len(vectors.shape) != 2:
+        print("Specify a valid dataset first.")
+        exit()
 
-vectors = None # Collection of D n-dimensional vectors. Must be of shape (D, n)
+    N, C = vectors.shape # Number of vectors and their dimensionality (C for "channels").
+    K = 3 # cluster counts
+    n = 7 # hardware precision of annealer coupling / bias
+    LAMBDA = (2 ** (n-1) - 1) / (2 * (K - 2 if K > 2 else K)) # calculate lambda cluster constraint
+    SCALE = 1 # scale loss by...
 
-# Parse data file.
-fi = open(path.join("datasets", DATA))
-lines = [line.strip().split(',') for line in fi.readlines() if line.strip() != '']
+    PARAM_MIN = 0 # minimum value of param
+    PARAM_MAX = 1 # maximum value of param
+    STEP = 0.1 # step to vary param by
 
-values = [[float(entry) for entry in line[:-1]] for line in lines]
-name_numbers = { 'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2 }
-labels = [name_numbers[line[-1]] for line in lines]
+    peaks = [] # record all peak accuracies
+    scales = [] # record associated scales
+    best_assignments = [] # record best clusterings
 
-values = np.array(values)
+    for SCALE in tqdm(np.arange(PARAM_MIN, PARAM_MAX, STEP)):
+        print("Scaling all loss to %f" % SCALE)
 
-# "Load" parsed dataset.
-vectors = values
+        diffs = np.zeros((N, N)) # store pairwise differences
+        sums = np.zeros(N) # store sum of all pairwise differences relative to i-th pixel
 
-if vectors is None or len(vectors.shape) != 2:
-    print("Specify a valid dataset first.")
-    exit()
+        print("Calculating pairwise differences...")
 
-N, C = vectors.shape # Number of vectors and their dimensionality (C for "channels").
-K = 3 # cluster counts
-n = 7 # hardware precision of annealer coupling / bias
-LAMBDA = (2 ** (n-1) - 1) / (2 * (K - 2 if K > 2 else K)) # calculate lambda cluster constraint
-SCALE = 1 # scale loss by...
+        for idx, vec in tqdm(enumerate(vectors)): # for all pixels in the image...
+            diffs[idx] = np.linalg.norm((vectors - vec), axis=1) * SCALE # assign pairwise difference for all pixels, scaled accordingly
+            sums[idx] = np.sum(diffs[idx]) # assign sum of all pairwise differences relative to i-th pixel
 
-PARAM_MIN = 0 # minimum value of param
-PARAM_MAX = 1 # maximum value of param
-STEP = 0.1 # step to vary param by
+        h = {} # intrinsic weight matrix
+        J = {} # coupling weight matrix
 
-peaks = [] # record all peak accuracies
-scales = [] # record associated scales
+        print("Assigning pairwise clustering weights...")
 
-for SCALE in tqdm(np.arange(PARAM_MIN, PARAM_MAX, STEP)):
-    print("Scaling all loss to %f" % SCALE)
+        # assign general pairwise clustering weights - both biases and couplings
+        for k in tqdm(range(K)): # for every cluster we have...
+            for i in range(N): # for every pixel in the image...
+                for j in range(N): # and every pixel it might be paired with...
+                    # give each binary variable an unique number
+                    var_i = 10 * i + k # s_ik = x_(10i+k)
+                    var_j = 10 * j + k # s_jk = x_(10j+k)
+                    if i == j: # intrinsic weights (bias) indexing - for every cluster assignment (s_ik for all k in K)
+                        h[var_i] = sums[i] + (2 * LAMBDA * (K - 2 if K > 2 else K)) # assign weight
+                    else:
+                        J[(var_i, var_j)] = 0.5 * diffs[i][j] # the weighting for a cluster assignment for two vars is half their "distance"
 
-    diffs = np.zeros((N, N)) # store pairwise differences
-    sums = np.zeros(N) # store sum of all pairwise differences relative to i-th pixel
+        print("Assigning pairwise cluster possibility weights...")
 
-    print("Calculating pairwise differences...")
+        # assign pairwise clustering variables per pixel
+        for i in tqdm(range(N)):
+            for a in range(K):
+                for b in range(K):
+                    if a != b:
+                        var_a = 10 * i + a
+                        var_b = 10 * i + b
+                        J[(var_a, var_b)] = LAMBDA
 
-    for idx, vec in tqdm(enumerate(vectors)): # for all pixels in the image...
-        diffs[idx] = np.linalg.norm((vectors - vec), axis=1) * SCALE # assign pairwise difference for all pixels, scaled accordingly
-        sums[idx] = np.sum(diffs[idx]) # assign sum of all pairwise differences relative to i-th pixel
+        # TODO: Guarantee that for all s_ik for each i, only one is "true"
 
-    h = {} # intrinsic weight matrix
-    J = {} # coupling weight matrix
+        print("%d couplings defined between %d variables." % (len(J.keys()), N * K))
 
-    print("Assigning pairwise clustering weights...")
+        response = QBSolv().sample_ising(h, J, verbosity=2) # sample / solve Ising problem
 
-    # assign general pairwise clustering weights - both biases and couplings
-    for k in tqdm(range(K)): # for every cluster we have...
-        for i in range(N): # for every pixel in the image...
-            for j in range(N): # and every pixel it might be paired with...
-                # give each binary variable an unique number
-                var_i = 10 * i + k # s_ik = x_(10i+k)
-                var_j = 10 * j + k # s_jk = x_(10j+k)
-                if i == j: # intrinsic weights (bias) indexing - for every cluster assignment (s_ik for all k in K)
-                    h[var_i] = sums[i] + (2 * LAMBDA * (K - 2 if K > 2 else K)) # assign weight
-                else:
-                    J[(var_i, var_j)] = 0.5 * diffs[i][j] # the weighting for a cluster assignment for two vars is half their "distance"
+        print(response)
 
-    print("Assigning pairwise cluster possibility weights...")
+        clusters = list(response.samples()) # store clustering assignments
+        clustered = np.zeros(N)
 
-    # assign pairwise clustering variables per pixel
-    for i in tqdm(range(N)):
-        for a in range(K):
-            for b in range(K):
-                if a != b:
-                    var_a = 10 * i + a
-                    var_b = 10 * i + b
-                    J[(var_a, var_b)] = LAMBDA
+        accuracies = [] # record accuracies of every given solution
+        assignments = [] # record corresponding assignments
 
-    # TODO: Guarantee that for all s_ik for each i, only one is "true"
+        possible_labels = set(permutations(range(K))) # generate all possible permutations of clusters
 
-    print("%d couplings defined between %d variables." % (len(J.keys()), N * K))
+        for i, clustering in enumerate(clusters): # for every low-energy solution...
+            curr_max = float('-inf') # current max accuracy achieved with permutation
+            clustered = np.zeros(N) # initialize empty array
 
-    response = QBSolv().sample_ising(h, J, verbosity=2) # sample / solve Ising problem
+            # mark cluster assignments for visualization
+            for key in clustering.keys():
+                # extract pixel number and cluster assignments (var. number format is {pixel}{cluster})
+                key_str = str(key)
+                # since pixel #0 cluster assignments will break this format (only {cluster}), fix manually
+                if len(key_str) == 1:
+                    key_str = "0%s" % key_str
+                # extract all parameters from single number!
+                key_pix, key_cluster, key_status = int(key_str[:-1]), int(key_str[-1:]), clustering[key]
 
-    print(response)
+                for label_set in possible_labels:
+                    # if spin / state is +1, fill in cluster indicated by variable
+                    if key_status == 1:
+                        clustered[key_pix] = label_set[key_cluster] # replace with permuted cluster label possibility
 
-    clusters = list(response.samples()) # store clustering assignments
-    clustered = np.zeros(N)
+                    # Count matches
+                    matches = np.sum(np.where(clustered == labels, 1, 0))
+                    percent = matches / N
+                
+                    # Update maximum accuracy if this permutation is better
+                    if percent > curr_max: curr_max = percent
 
-    accuracies = [] # record accuracies of every given solution
-    assignments = [] # record corresponding assignments
+            # Record accuracy
+            accuracies.append(curr_max)
 
-    possible_labels = set(permutations(range(K))) # generate all possible permutations of clusters
+            # Record assignment
+            assignments.append(clustered)
 
-    for i, clustering in enumerate(clusters): # for every low-energy solution...
-        curr_max = float('-inf') # current max accuracy achieved with permutation
-        clustered = np.zeros(N) # initialize empty array
+        accuracies = np.array(accuracies)
 
-        # mark cluster assignments for visualization
-        for key in clustering.keys():
-            # extract pixel number and cluster assignments (var. number format is {pixel}{cluster})
-            key_str = str(key)
-            # since pixel #0 cluster assignments will break this format (only {cluster}), fix manually
-            if len(key_str) == 1:
-                key_str = "0%s" % key_str
-            # extract all parameters from single number!
-            key_pix, key_cluster, key_status = int(key_str[:-1]), int(key_str[-1:]), clustering[key]
+        print(accuracies)
 
-            for label_set in possible_labels:
-                # if spin / state is +1, fill in cluster indicated by variable
-                if key_status == 1:
-                    clustered[key_pix] = label_set[key_cluster] # replace with permuted cluster label possibility
+        SNR = np.linalg.norm(np.mean(np.mean(vectors, axis=0), axis=0)) / np.linalg.norm(np.std(np.std(vectors, axis=0), axis=0))
+        print("Found input SNR to be %f." % SNR)
 
-                # Count matches
-                matches = np.sum(np.where(clustered == labels, 1, 0))
-                percent = matches / N
-            
-                # Update maximum accuracy if this permutation is better
-                if percent > curr_max: curr_max = percent
+        peak = np.max(accuracies) * 100
+        best = assignments[np.argmax(accuracies)]
 
-        # Record accuracy
-        accuracies.append(curr_max)
+        print("Highest accuracy: %f percent" % peak)
+        print("Associated clustering: %s" % str(best))
 
-        # Record assignment
-        assignments.append(clustered)
+        peaks.append(peak)
+        scales.append(SCALE)
+        best_assignments.append(best)
 
-    accuracies = np.array(accuracies)
+    print(peaks)
+    print(scales)
 
-    print(accuracies)
+    # Find best-fit polynomial for data
+    # poly = np.poly1d(np.polyfit(scales, peaks, 10))
+    # param_x = np.linspace(PARAM_MIN, PARAM_MAX, 100)
 
-    SNR = np.linalg.norm(np.mean(np.mean(vectors, axis=0), axis=0)) / np.linalg.norm(np.std(np.std(vectors, axis=0), axis=0))
-    print("Found input SNR to be %f." % SNR)
+    # Plot accuracy points and line of best fit
+    plt.scatter(scales, peaks)
+    # plt.plot(param_x, poly(param_x), 'r--')
+    plt.ylim(0,100)
 
-    peak = np.max(accuracies) * 100
+    plt.xticks(np.arange(PARAM_MIN, PARAM_MAX, STEP))
+    plt.yticks(np.arange(0, 100, 10))
 
-    print("Highest accuracy: %f percent" % peak)
-    print("Associated clustering: %s" % str(assignments[np.argmax(accuracies)]))
+    plt.xlabel("Scale")
+    plt.ylabel("Accuracy")
 
-    peaks.append(peak)
-    scales.append(SCALE)
+    if show_plot: plt.show()
 
-print(peaks)
-print(scales)
-
-# Find best-fit polynomial for data
-# poly = np.poly1d(np.polyfit(scales, peaks, 10))
-# param_x = np.linspace(PARAM_MIN, PARAM_MAX, 100)
-
-# Plot accuracy points and line of best fit
-plt.scatter(scales, peaks)
-# plt.plot(param_x, poly(param_x), 'r--')
-plt.ylim(0,100)
-
-plt.xticks(np.arange(PARAM_MIN, PARAM_MAX, STEP))
-plt.yticks(np.arange(0, 100, 10))
-
-plt.xlabel("Scale")
-plt.ylabel("Accuracy")
-
-plt.show()
+    return {
+        'bests': best_assignments,
+        'peaks': peaks,
+        'scales': scales,
+        # 'plot': plt.gcf()
+    }
